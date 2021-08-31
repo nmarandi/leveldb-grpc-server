0f160356453c83492ade7fc269ee12e1f31a1a3c
Milad Marandi <milad.marandi@aibox.com>
2021-08-31 09:48:37 +0430

    fix appveyor build for windows

--- include/config.h
+++ include/config.h
@@ -6,7 +6,11 @@ namespace leveldb
 {
 namespace config
 {
+#if defined(_WIN64) || defined(WIN64) || defined(_WIN32) || defined(WIN32)
+const std::string db_file_base_path = "c:\\Users\\Public\\";
+#else
 const std::string db_file_base_path = "/tmp/";
+#endif
 }
 }
 
